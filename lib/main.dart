51398603ee8127e34c66551b18381326fac43123
Pedro Zwang <pedroh.zwang@gmail.com>
2022-08-26 00:08:04 -0300

    componente statefull e controle de estado

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
   void decrement() {
-    print('decrement');
+    setState(() {
+      counter--;
+    });
   }
 
   void increment() {
-    print('increment');
+    setState(() {
+      counter++;
+    });
   }
+
+  bool get isFull => counter == 20;
+  bool get isEmpty => counter <= 0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,51 +54,51 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Pode entrar!',
+            Text(isFull ? 'Lotado!' : 'Pode entrar!',
                 style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400)),
-            const Padding(
-              padding: EdgeInsets.all(60),
-              child: const Text('0',
+                  fontSize: 30,
+                  color: isFull ? Colors.red : Colors.white,
+                  fontWeight: FontWeight.w400,
+                )),
+            Padding(
+              padding: const EdgeInsets.all(60),
+              child: Text('$counter',
                   style: TextStyle(
                       fontSize: 90,
-                      color: Colors.white,
+                      color: isFull ? Colors.red : Colors.white,
                       fontWeight: FontWeight.w400)),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextButton(
-                  onPressed: decrement,
+                  onPressed: isEmpty ? null : decrement,
                   style: TextButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      fixedSize: const Size(100, 100),
-                      primary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      )),
-                  child: const Text('Saiu',
+                    backgroundColor: Colors.transparent,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: Text('Saiu',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: isEmpty ? Colors.red : Colors.white,
                         fontSize: 16,
                         fontWeight: FontWeight.w800,
                       )),
                 ),
                 const SizedBox(width: 32),
                 TextButton(
-                  onPressed: increment,
+                  onPressed: isFull ? null : increment,
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.transparent,
                       fixedSize: const Size(100, 100),
-                      primary: Colors.white,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(24),
                       )),
-                  child: const Text('Entrou',
+                  child: Text('Entrou',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: isFull ? Colors.red : Colors.white,
                         fontSize: 16,
                         fontWeight: FontWeight.w800,
                       )),
